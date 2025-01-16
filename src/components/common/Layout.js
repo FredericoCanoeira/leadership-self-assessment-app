f54569c99cf4ddb1dd92bc85ef59acf2cad2e6d9
Frederico Canoeira <fcanoeira@gmail.com>
2025-01-16 01:38:53 -0500

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,58 +1,39 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import "./Layout.css"; 
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+
+
   return (
     <div>
       {/* Cabeçalho */}
-      <header style={styles.header}>
-        <h1>Everest Assessment</h1>
-        <nav style={styles.nav}>
-          <a href="/home" style={styles.link}>Início</a>
-          <a href="/Dashboard" style={styles.link}>Dashboard</a>
-          <a href="/assessment" style={styles.link}>Realizar o teste</a>
-          <a href="/results" style={styles.link}>Ranking</a>
-          <a href="/results" style={styles.link}>Resultados</a>
-          <a href="/" style={styles.link}>Login</a>
-          <a href="/register" style={styles.link}>Registrar</a>
+      <header className="header">
+          {/*<h1>Everest Assessment</h1>*/}
+        <nav className="nav">
+           {/* Exibe "Realizar o teste" somente na página inicial ("/") */}
+            {location.pathname === "/" && (
+            <a href="/assessment" className="link">Realizar o teste</a>
+          )}
+        {/*<a href="/home" style={styles.link}>Início</a>*/}
+            {/*<a href="/Dashboard" style={styles.link}>Dashboard</a>*/}
+            {/*<a href="/results" style={styles.link}>Ranking</a>*/}
+            {/*<a href="/results" style={styles.link}>Resultados</a>*/}
+            {/*<a href="/" style={styles.link}>Login</a>*/}
+            {/*<a href="/register" style={styles.link}>Registrar</a>*/}
         </nav>
       </header>
 
       {/* Conteúdo */}
-      <main style={styles.main}>{children}</main>
+      <main className="main">{children}</main>
 
       {/* Rodapé */}
-      <footer style={styles.footer}>
-        <p>© 2025 Everest Assessment</p>
+      <footer className="footer">
+        <p>© 2025 Everest - Consultoria e Gestão de Carreiras, LDA</p>
       </footer>
     </div>
   );
-};
-
-const styles = {
-  header: {
-    backgroundColor: '#f8f9fa',
-    padding: '10px',
-    borderBottom: '1px solid #ddd',
-  },
-  nav: {
-    display: 'flex',
-    gap: '10px',
-    marginTop: '10px',
-  },
-  link: {
-    textDecoration: 'none',
-    color: '#007bff',
-  },
-  main: {
-    padding: '20px',
-    minHeight: '80vh',
-  },
-  footer: {
-    textAlign: 'center',
-    padding: '10px',
-    backgroundColor: '#f8f9fa',
-    borderTop: '1px solid #ddd',
-  },
 };
 
 export default Layout;
